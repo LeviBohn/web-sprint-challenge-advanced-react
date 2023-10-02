c471ef0e368dfd76d952b87c27f811718195e678
Levi Bohn <levibohn@gmail.com>
2023-10-02 05:40:17 -0500

    MVP complete

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,9 @@
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import AppClass from './AppClass';
+import { fireEvent } from '@testing-library/react';
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
